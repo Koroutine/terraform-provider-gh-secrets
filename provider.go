231ea69edc6d84cc1bc9b7f94c08e0a0781bd681
Drache93 <dominic@koroutine.tech>
2021-11-02 20:38:44 +0000

    Make token optinoal to enable

--- provider.go
+++ provider.go
@@ -15,7 +15,7 @@ func Provider() *schema.Provider {
 		Schema: map[string]*schema.Schema{
 			"token": {
 				Type:        schema.TypeString,
-				Required:    true,
+				Optional:    true,
 				Sensitive:   true,
 				DefaultFunc: schema.EnvDefaultFunc("GITHUB_TOKEN", nil),
 				Description: "The OAuth token used to connect to GitHub.",
@@ -33,10 +33,6 @@ func Provider() *schema.Provider {
 func config(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 
 	token := d.Get("token").(string)
-
-	if len(token) == 0 {
-		return nil, diag.Errorf("`GITHUB_TOKEN` env variable or `token` provider value are not set")
-	}
 
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
